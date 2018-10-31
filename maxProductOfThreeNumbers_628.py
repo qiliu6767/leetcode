b2fb9ab32c57153d7150fc6b7e7fd27e5c710b01
Xiaotangyuaner <42474583+qiliu6767@users.noreply.github.com>
2018-10-31 09:10:45 -0500

    Update maxProductOfThreeNumbers_628.py

--- maxProductOfThreeNumbers_628.py
+++ maxProductOfThreeNumbers_628.py
@@ -1,2 +1,11 @@
-import math
-print(int(math.ceil(math.sqrt(4))))
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return max(nums[0] * nums[1] * nums[-1], nums[-3] * nums[-2] * nums[-1])
+    
+        
+        
